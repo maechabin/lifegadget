3a6e17e8e1a18f1d25882ce692f9e32d0194c3d1
maechabin <mitoppo@gmail.com>
2019-04-13 14:05:44 +0900

    Linkをreact-router-domから読み込むように修正

--- src/widgets/CategoryButton.jsx
+++ src/widgets/CategoryButton.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const CategoryButton = (props) => {
   return (
     <ul className="widget__category-button">
-      <li><Link to="/">新着</Link></li>
-      <li><Link to="/category/18">ライフ<span>ハック</span></Link></li>
-      <li><Link to="/category/14">本日の<span>アイテム</span></Link></li>
-      <li><Link to="/category/2">お知らせ</Link></li>
+      <li>
+        <Link to="/">新着</Link>
+      </li>
+      <li>
+        <Link to="/category/18">
+          ライフ<span>ハック</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/category/14">
+          本日の<span>アイテム</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/category/2">お知らせ</Link>
+      </li>
     </ul>
   );
 };
