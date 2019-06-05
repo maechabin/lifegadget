f14ac8457b49551c3d66f33d7cdc591a3f75825f
maechabin <mitoppo@gmail.com>
2019-06-05 22:42:48 +0900

    ローディング画面のスタイル修正

--- src/shared/Loading.tsx
+++ src/shared/Loading.tsx
@@ -6,13 +6,27 @@ type PropsType = {
 };
 
 function Loading({ isLoading, children }: PropsType): JSX.Element {
+  const style = {
+    loading: {
+      padding: '80px 0',
+      textAlign: 'center',
+    } as React.CSSProperties,
+  };
+
   const childComponent = isLoading ? (
-    <img src="../images/loading.svg" alt="loading..." />
+    <div style={style.loading}>
+      <img
+        src={`${process.env.REACT_APP_IMAGE_PATH}/loading.svg`}
+        alt="loading..."
+        width="100"
+        height="100"
+      />
+    </div>
   ) : (
     children
   );
 
-  return <div className="article__loading">{childComponent}</div>;
+  return <div>{childComponent}</div>;
 }
 
 export default Loading;
